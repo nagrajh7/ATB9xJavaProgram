8daa5e5dbe412a65f35563243d06068015342c4d
Nagaraj <nhnagaraj9@gmail.com>
2024-11-02 16:33:22 +0530

    task3

---
+++ src/Task_03102024/task3.java
@@ -0,0 +1,12 @@
+package Task_03102024;
+
+public class task3 {
+    public static void main(String[] args) {
+        System.out.println(10+10);
+        System.out.println(10-10);
+        System.out.println(10*10);
+        System.out.println(10/10);
+
+    }
+
+}
