15d381cf14b4d4825109a96baeffa747b4ba86a5
Nagaraj <nhnagaraj9@gmail.com>
2024-11-02 16:12:52 +0530

    Initial commit my files

---
+++ src/Oct/exrcs_02102024/Lab002.java
@@ -0,0 +1,4 @@
+package Oct.exrcs_02102024;
+
+public class Lab002 {
+}
---
+++ src/Oct/exrcs_04102024_Main_method/Lab004.java
@@ -0,0 +1,7 @@
+package Oct.exrcs_04102024_Main_method;
+
+public class Lab004 {
+    public static void main(String[] args) {
+
+    }
+}
